b9a0f94c8e4e7ebd1cf7af88afe9f9662d0d21bc
admin <serg____@mail.ru>
2025-01-18 23:52:08 +0300

    Export fields

--- commonlib/container.go
+++ commonlib/container.go
@@ -7,49 +7,49 @@ import (
 
 // Container with Hashmap and Expiration
 type Container struct {
-	storage map[any]time.Time
-	mu      sync.Mutex
-	ttl     time.Duration
+	InternalMap map[any]time.Time
+	Mu          sync.Mutex
+	ttl         time.Duration
 }
 
 // Constructor
 func NewContainer(ttl time.Duration) *Container {
 	var c Container
-	c.storage = make(map[any]time.Time)
+	c.InternalMap = make(map[any]time.Time)
 	c.ttl = ttl
 	return &c
 }
 
 // Add - add to container
 func (c *Container) Add(value any) {
-	c.mu.Lock()
-	c.storage[value] = time.Now().Add(c.ttl)
-	c.mu.Unlock()
+	c.Mu.Lock()
+	c.InternalMap[value] = time.Now().Add(c.ttl)
+	c.Mu.Unlock()
 }
 
 // Delete - remove from container
 func (c *Container) Delete(value any) {
-	c.mu.Lock()
-	delete(c.storage, value)
-	c.mu.Unlock()
+	c.Mu.Lock()
+	delete(c.InternalMap, value)
+	c.Mu.Unlock()
 }
 
 // CheckExpired - wipe all expired
 func (c *Container) CheckExpired() {
 	now := time.Now()
-	c.mu.Lock()
-	for key, timeOfExpire := range c.storage {
+	c.Mu.Lock()
+	for key, timeOfExpire := range c.InternalMap {
 		if now.After(timeOfExpire) {
-			delete(c.storage, key)
+			delete(c.InternalMap, key)
 		}
 	}
-	c.mu.Unlock()
+	c.Mu.Unlock()
 }
 
 // Check - check value inside map
 func (c *Container) CheckInside(value any) bool {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	_, ok := c.storage[value]
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	_, ok := c.InternalMap[value]
 	return ok
 }
--- commonlib/container_list.go
+++ commonlib/container_list.go
@@ -6,35 +6,35 @@ import (
 
 // Container with hashset
 type Hashset struct {
-	storage map[any]bool
-	mu      sync.Mutex
+	InternalHashset map[any]bool
+	Mut             sync.Mutex
 }
 
 // Constructor
 func NewHashset() *Hashset {
 	var c Hashset
-	c.storage = make(map[any]bool)
+	c.InternalHashset = make(map[any]bool)
 	return &c
 }
 
 // Add - add to hashset
 func (c *Hashset) Add(value any) {
-	c.mu.Lock()
-	c.storage[value] = true
-	c.mu.Unlock()
+	c.Mut.Lock()
+	c.InternalHashset[value] = true
+	c.Mut.Unlock()
 }
 
 // Delete - remove from hashset
 func (c *Hashset) Delete(value any) {
-	c.mu.Lock()
-	delete(c.storage, value)
-	c.mu.Unlock()
+	c.Mut.Lock()
+	delete(c.InternalHashset, value)
+	c.Mut.Unlock()
 }
 
 // Check - check value inside hashset
 func (c *Hashset) CheckInside(value any) bool {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	_, ok := c.storage[value]
+	c.Mut.Lock()
+	defer c.Mut.Unlock()
+	_, ok := c.InternalHashset[value]
 	return ok
 }
